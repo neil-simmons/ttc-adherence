26c6ae7338ae1f43a816a1f05fa6ddcce037a6ea
neil-simmons <neilsimmonsca@gmail.com>
2026-05-19 19:41:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1007,7 +1007,7 @@ def render_filter_panel(available_routes, parquet_path, trips, stop_times, stops
 # 7. MAIN UI & TAB LAYOUT
 # ==============================================================================
 st.title("TTC ScheduleWatch")
-st.caption("Open-data analysis tool of TTC streetcar performance versus published schedules (GTFS). Visualizes streetcar GPS between March 15 - May 2 2026")
+st.caption("Open-data analysis tool of TTC streetcar performance versus published schedules (GTFS). Visualizes streetcar locations between March 15 - May 2 2026")
 
 parquet_path = get_parquet_path()
 available_routes = get_available_routes(parquet_path)
