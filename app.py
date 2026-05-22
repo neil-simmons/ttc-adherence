53a7611371bc1e90c5f187c14d83240cfa2c4486
neil-simmons <neilsimmonsca@gmail.com>
2026-05-21 22:37:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import gpd as gpd
 import geopandas as gpd
 from shapely.geometry import LineString, Point
 from shapely.ops import substring, linemerge
